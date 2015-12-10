091f53fb56135537c13c905ef16629874cc6c94e
unknown <yuli@yuli.longtu-inc.com>
2015-12-10 15:38:50 +0800

    新建jade学习文件夹

---
+++ jade/server.js
@@ -0,0 +1,17 @@
+//server.js
+var http= require('http');
+var jade=require('jade');
+http.createServer(function(req,res){
+	//console.log(res);
+	// res.writeHead(200, {'Content-Type': 'text/plain'});//返回html
+	res.writeHead(200, {'Content-Type': 'text/html'});//返回页面
+	//1.compile
+	// var fn=jade.compile('div #{course}',{});
+	// var html=fn({course:'jade'});
+	//2：jade.render
+	// var html=jade.render('div #{course}',{course:'jade render'});
+	//3:jade.renderFile
+	var html = jade.renderFile('index.jade',{course:'jade renderFile',pretty:true});
+	res.end(html);
+}).listen('1337');
+console.log('server running');
